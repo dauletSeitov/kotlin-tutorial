60defb0b4eb0ac8c896ca368a5ea72a1dd04d9a4
phantom <b.seilov@alabs.team>
2024-04-26 19:02:22 +0200

    tutorial thenewboston lesson 1

---
+++ src/test/kotlin/kotli/tutorial/springboot/SpringbootApplicationTests.kt
@@ -0,0 +1,13 @@
+package kotli.tutorial.springboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
