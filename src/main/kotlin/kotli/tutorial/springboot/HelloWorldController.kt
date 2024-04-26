1674edf3a4e7e027b1c36ffb05e79e7d3c3f0663
phantom <b.seilov@alabs.team>
2024-04-26 19:07:40 +0200

    tutorial thenewboston lesson 2

---
+++ src/main/kotlin/kotli/tutorial/springboot/HelloWorldController.kt
@@ -0,0 +1,16 @@
+package kotli.tutorial.springboot
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class HelloWorldController {
+
+
+    @GetMapping("/hello")
+    fun hello() = "hello"
+
+
+}
